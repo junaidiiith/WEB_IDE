4a20b73286cf41d8495cd8ba865e0e51c639dad2
shivtej1505 <15electronicmotor@gmail.com>
2015-10-06 16:26:11 +0530

    json serialization and data retrival from db done

--- src/data_project/data1/data1/urls.py
+++ src/data_project/data1/data1/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^table/', include('table.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- src/data_project/data1/table/views.py
+++ src/data_project/data1/table/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
+from .models import table
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hwello")
+    data = serializers.serialize("json",table.objects.all())
+    return HttpResponse(data)
