78d42f786dc3a2f196664e9830796b891def7339
Raj Manvar <rajmanvarchamp@gmail.com>
2015-10-07 12:35:22 +0530

    java ans c data added into data insertion app

--- source/data_project/data1/table/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.core import serializers
-from .models import table
-
-# Create your views here.
-def index(request):
-    data = serializers.serialize("json",table.objects.all())
-    return HttpResponse(data)
