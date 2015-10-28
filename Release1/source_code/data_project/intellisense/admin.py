855b293e77b87acf1fd55fbe718dd57e1568370c
Lashit Jain <jainlashit@gmail.com>
2015-10-28 20:06:43 +0530

    Release1 Directory removed

--- Release1/source_code/data_project/intellisense/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import c,csharp,java
-
-admin.site.register(c)
-admin.site.register(java)
-admin.site.register(csharp)
--- Release1/source_code/final_database/dataapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from dataapp.models import language_table,class_table,data_table
-admin.site.register(language_table)
-admin.site.register(class_table)
-admin.site.register(data_table)
