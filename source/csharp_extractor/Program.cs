a8c536beb673c5887715d82160d9625a68e8232f
Shalin Agrawal <shalint08@gmail.com>
2015-11-20 16:20:34 +0530

    SRS document updated

---
+++ source/csharp_extractor/Program.cs
@@ -0,0 +1,43 @@
+﻿// use this as program.cs file in monodevelop.
+// file will be generated at <project_folder>/<project_name>/<project_name>/bin/release/test.txt 
+
+
+using System;
+using System.Reflection;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+public struct method_list12
+{
+	public string method_name ;
+	public string return_type;
+	//public System.Reflection.ParameterInfo [] parameters;
+	public string [] parameters;
+	public string class_name;
+};
+
+namespace assembly
+{
+
+
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Assembly mscorlib = Assembly.LoadFrom("/usr/lib/mono/4.5/System.dll");
+			foreach (Type type in mscorlib.GetTypes())
+			{
+				foreach (MethodInfo method in type.GetMethods()) 
+				{
+					method_list12 a = new method_list12();
+					a.method_name = method.Name;
+					a.return_type = method.ReturnType.ToString();
+					a.class_name = type.FullName;
+					var json = JsonConvert.SerializeObject (a);
+					Console.WriteLine (json);
+					System.IO.File.AppendAllText ("test.txt" ,json);
+				}
+			}
+		}
+	}
+}
