71318bf5f096735355299dbb203f4b7a2cd1ff9c
shivtej <shivang.nagaria@gmail.com>
2015-11-20 22:05:25 +0530

    database feeder done

---
+++ source/Extractor/Json_data/c_cpp_libraries/setjmp.h
@@ -0,0 +1,10 @@
+[{
+  "method_name" : "setjmp",
+  "return_value" : "int",
+  "arguments" : "jmp_buf __env"
+},
+{
+  "method_name" : "siglongjmp",
+  "return_value" : "void",
+  "arguments" : "sigjmp_buf __env, int __val"
+}]
--- source/Extractor/libraries/errno.h
+++
@@ -1,5 +0,0 @@
-[{
-  "method_name" : "__errno_location",
-  "return_value" : "int*",
-  "arguments" : "void"
-}]
